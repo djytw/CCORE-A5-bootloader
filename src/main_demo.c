07871f92e4234872078d50f9d2ef097757f8b42a
djytw <djytw@live.com>
2018-08-20 02:35:04 +0800

    starts flash writing

--- src/main_demo.c
+++ src/main_demo.c
@@ -4,7 +4,7 @@
 #include "eport_api.h"
 #include "eflash_api.h"
 #include "eflash_demo.h"
-
+#define __VER__  "\033[33mdjytw C*Core bootloader.\n\rVersion: 0.1 alpha\n\r\n\r\033[0m"
 void test();
 void printf_version(void){
 	MSG("\033[2J\n\r\033[32m"
@@ -20,6 +20,36 @@ void printf_version(void){
 	   				 );
 }
 u8 sci_read_notimeout();
+u8 direct_write(u32 ds, u32 dl){
+	u32 efm_clk,i,ret_code;
+	efm_clk = cpm_get_efmclk();
+	eflash_init(efm_clk);
+	for(i = ds; i <= ds+dl; i += 512){
+		ret_code = eflash_page_erase(i);
+		if(ret_code == EFLASH_PROG_ERASE_FAIL){
+			asm("bkpt");
+		}
+	}
+	i=ds;
+	while(i<ds+dl){
+		u32 data=0;u8 a;
+		a=sci_receive_byte();
+		data|=a;
+		a=sci_receive_byte();
+		data|=a<<8;
+		a=sci_receive_byte();
+		data|=a<<16;
+		a=sci_receive_byte();
+		data|=a<<24;
+
+		ret_code = eflash_program(i, data);
+		if(ret_code == EFLASH_PROG_ERASE_FAIL){
+			asm("bkpt");
+		}
+		i+=4;
+	}
+	return 0;
+}
 int main(void){
 	cpm_init();
 	sci_init(SCI_ID2, g_ips_clk, BAUDRATE_115200);
@@ -49,13 +79,15 @@ app_starts:
 
 	//receive cmd. Stop timing and proc.
 prog:
+	asm("nop");
+	u32 ds,dl;
+	ds=0x00420000;
+	dl=0;
 	while(1){
 		switch(ser){
 		case 'h':
 			//help cmd
-			MSG("\033[33mdjytw C*Core bootloader.\n\r"
-					"Version: 0.1 alpha\n\r"
-					"\n\r\033[0m"
+			MSG(__VER__
 					"Usage:\n\r"
 					"\ti  -  View chip info\n\r"
 					"\th  -  Show this help\n\r"
@@ -73,6 +105,61 @@ prog:
 		case 'b':
 			goto app_starts;
 			break;
+		case 'i':
+			MSG(__VER__);
+			break;
+		case 's':
+			ds=0;
+			MSG("New start point: ");
+			for(i=0;i<8;i++){
+				u8 t;
+				t=sci_receive_byte();
+				MSG("%c",t);
+				ds*=16;
+				if(t>='0'&&t<='9'){
+					ds+=t-'0';
+				}else if(t>='a'&&t<='f'){
+					ds+=t-'a'+10;
+				}else if(t>='A'&&t<='F'){
+					ds+=t-'A'+10;
+				}else{
+					MSG("\n\rMalformed command. Reset start point.\n\r");
+					ds=0x00420000;
+					break;
+				}
+			}
+			MSG("\n\rStart point = 0x%08x now.\n\r",ds);
+			break;
+		case 'l':
+			dl=0;
+			MSG("Enter data length: ");
+			for(i=0;i<8;i++){
+				u8 t;
+				t=sci_receive_byte();
+				MSG("%c",t);
+				dl*=16;
+				if(t>='0'&&t<='9'){
+					dl+=t-'0';
+				}else if(t>='a'&&t<='f'){
+					dl+=t-'a'+10;
+				}else if(t>='A'&&t<='F'){
+					dl+=t-'A'+10;
+				}else{
+					MSG("\n\rMalformed command.\n\r");
+					dl=0;
+					break;
+				}
+			}
+			if(dl==0||dl%4)
+				MSG("\n\rSet length failed. length should be divisible by 4 and not 0\n\r");
+			else
+				MSG("\n\rSet length ok. New value is 0x%08x.\n\r",dl);
+			break;
+		case 'w':
+			direct_write(ds,dl);
+			break;
+		default:
+			MSG("Unrecognized command - %c, Use 'h' to get help.\n\r",ser);
 		}
 		MSG("\n\rbootloader> ");
 		//will block
