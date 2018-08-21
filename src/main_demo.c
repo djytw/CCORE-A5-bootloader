087f9861e605ab263a9cfe94b9c8910fff47e654
djytw <djytw@live.com>
2018-08-21 23:49:30 +0800

    tidy main_demo.c

--- src/main_demo.c
+++ src/main_demo.c
@@ -5,8 +5,36 @@
 #include "eflash_api.h"
 #include "eflash_demo.h"
 #define __VER__  "\033[33mdjytw C*Core bootloader.\n\rVersion: 0.1 alpha\n\r\n\r\033[0m"
+#define DEBUG_BREAK asm("bkpt")
+#define PMSG(...) if(!is_programmer)MSG(__VA_ARGS__)
+#define DMSG(...) if(is_programmer)MSG(__VA_ARGS__)
 
 
+
+
+// is_programmer: 0: command user interface/ 1:programmer interface
+u8 is_programmer=0;
+
+// ds = download start point;  dl=data_length
+u32 ds,dl;
+
+void show_help(){
+	//show help msg. only in cui mode
+	PMSG(__VER__
+			"Usage:\n\r"
+			"\ti  -  View chip info\n\r"
+			"\th  -  Show this help\n\r"
+			"\ts  -  Set writing start point. \n\r");
+	PMSG("\t\tFollowed by address which will be changed. 8 digits exactly, without '0x'. The default value is 00420000.\n\r"
+			"\t\teg. s00420000\n\r"
+			"\tl  -  Set writing length.\n\r");
+	PMSG("\t\tFollowed by data length (or file size of the bin file), 3 digits exactly, less than 200. Default: 200\n\r"
+			"\tw  -  Direct writing to flash. \n\r");
+	PMSG("\t\tNeed to specify data length with 'l' command. Following LENGTH bytes will be write to flash.\n\r"
+			"\tc  -  Writing with SHA-256 check\n\r");
+	PMSG("\t\tNeed to specify data length with 'l' command. Following 256 bytes is SHA-256 value of data, and then LENGTH bytes will be write to flash and be checked.\n\r"
+			"\tb  -  Boot to program directly.\n\r\n\r");
+}
 void printf_version(void){
 	MSG("\033[2J\n\r\033[32m"
 	   				 "\n\r"
@@ -21,21 +49,97 @@ void printf_version(void){
 	   				 );
 }
 u8 sci_read_notimeout();
-u8 direct_write(u32 ds, u32 dl){
-	u32 efm_clk,i,ret_code;
+u8 set_data_length(){
+	dl=0;
+	PMSG("Enter data length: ");
+	//data_length: 4digits
+	for(i=0;i<4;i++){
+		u8 t;
+		t=sci_receive_byte();
+		PMSG("%c",t);
+		dl*=16;
+		if(t>='0'&&t<='9'){
+			dl+=t-'0';
+		}else if(t>='a'&&t<='f'){
+			dl+=t-'a'+10;
+		}else if(t>='A'&&t<='F'){
+			dl+=t-'A'+10;
+		}else{
+			PMSG("\n\rMalformed command.\n\r");
+			DMSG("f1");
+			dl=0;
+			return 1;
+		}
+	}
+	if(dl==0||dl%4){
+		PMSG("\n\rSet length failed. length should not be 0 and should be divisible by 4 \n\r");
+		DMSG("f2");
+		return 2;
+	}
+	PMSG("\n\rSet length ok. New value is 0x%08x.\n\r",dl);
+	DMSG("o%08x",dl);
+	return 0;
+}
+u8 set_start_point(){
+	ds=0;
+	PMSG("New start point: ");
+	//start_point: u32 : 8digits
+	for(i=0;i<8;i++){
+		u8 t;
+		t=sci_receive_byte();
+		PMSG("%c",t);
+		ds*=16;
+		if(t>='0'&&t<='9'){
+			ds+=t-'0';
+		}else if(t>='a'&&t<='f'){
+			ds+=t-'a'+10;
+		}else if(t>='A'&&t<='F'){
+			ds+=t-'A'+10;
+		}else{
+			PMSG("\n\rMalformed command. Reset start point.\n\r");
+			DMSG("f1");
+			ds=0x00420000;
+			return 1;
+		}
+	}
+	//check if ds is illegal
+	if(ds<0x00420000||ds>0x004f0000||ds%4){
+		PMSG("\n\rStart point should great than 0x00420000 and less than 0x004f0000, and divisble by 4. Start point is reset now.\n\r");
+		DMSG("f2");
+		ds=0x00420000;
+		return 2;
+	}
+	PMSG("\n\rStart point = 0x%08x now.\n\r",ds);
+	DMSG("o%08x",ds);
+	return 0;
+}
+u8 direct_write(){
+	//Init flash clock.
+	u32 efm_clk;
 	efm_clk = cpm_get_efmclk();
 	eflash_init(efm_clk);
 
-	if(!dl||dl%4){
-		asm("bkpt");
+	if(dl==0){
+		PMSG("Data length should be set first!\n\r");
+		DMSG("f1");
+		return 1;
 	}
+	//dl should not be illegal
+	assert(dl&&!(dl%4));
+
+	//malloc not working. Use sram addr directly.
 	u32 *bin=(u32*)0x00820000;
+
+	//erase flash from ds to ds+dl
 	for(i = ds; i <= ds+dl; i += 512){
 		ret_code = eflash_page_erase(i);
 		if(ret_code == EFLASH_PROG_ERASE_FAIL){
-			asm("bkpt");
+			PMSG("Flash erase error!\n\r");
+			DMSG("f2");
+			return 2;
 		}
 	}
+	//receive data
 	for(i=0;i<dl/4;i++){
 		u32 data=0;u8 a;
 		a=sci_receive_byte();
@@ -48,10 +152,15 @@ u8 direct_write(u32 ds, u32 dl){
 		data|=a<<24;
 		bin[i]=data;
 	}
+	//program at once
 	ret_code = eflash_bulk_program(ds,dl/4,bin);
 	if(ret_code == EFLASH_PROG_ERASE_FAIL){
-		asm("bkpt");
+		PMSG("Flash write error!\n\r");
+		DMSG("f3");
+		return 3;
 	}
+	PMSG("Flash write ok!\n\r");
+	DMSG("o");
 	return 0;
 }
 int main(void){
@@ -61,13 +170,19 @@ int main(void){
 	printf_version();
 	u32 i,j;
 	u8 ser;
+	is_programmer=0;
 	for(i=20;i>0;i--){
 		MSG("\rSystem will boot in %ds...",i);
 		for(j=0;j<1000;j++){
 			ser=sci_read_notimeout();
 			if(ser!=0xff){
 				//receive cmd
-				MSG("\n\r");
+				if(ser=='p'){
+					//internal usage for programmers, will strip output msg.
+					is_programmer=1;
+					ser=sci_receive_byte();
+				}
+				PMSG("\n\r");
 				goto prog;
 			}
 			//TODO: a more precise delay
@@ -78,101 +193,41 @@ int main(void){
 	//time out. starts the program.
 	//app_starts()
 app_starts:
-	MSG("Starting app.\n\r");
+	PMSG("Starting app.\n\r");
 	while(1);
 
 	//receive cmd. Stop timing and proc.
 prog:
-	asm("nop");
-	u32 ds,dl;
 	ds=0x00420000;
 	dl=0;
 	while(1){
 		switch(ser){
 		case 'h':
 			//help cmd
-			MSG(__VER__
-					"Usage:\n\r"
-					"\ti  -  View chip info\n\r"
-					"\th  -  Show this help\n\r"
-					"\ts  -  Set writing start point. \n\r");
-			MSG("\t\tFollowed by address which will be changed. 8 digits exactly, without '0x'. The default value is 00420000.\n\r"
-					"\t\teg. s00420000\n\r"
-					"\tl  -  Set writing length.\n\r");
-			MSG("\t\tFollowed by data length (or file size of the bin file), 3 digits exactly, less than 200. Default: 200\n\r"
-					"\tw  -  Direct writing to flash. \n\r");
-			MSG("\t\tNeed to specify data length with 'l' command. Following LENGTH bytes will be write to flash.\n\r"
-					"\tc  -  Writing with SHA-256 check\n\r");
-			MSG("\t\tNeed to specify data length with 'l' command. Following 256 bytes is SHA-256 value of data, and then LENGTH bytes will be write to flash and be checked.\n\r"
-					"\tb  -  Boot to program directly.\n\r\n\r");
-			break;
-		case 'p':
-			//internal usage for programmers, will strip output msg.
+			show_help();
 			break;
 		case 'b':
 			goto app_starts;
 			break;
 		case 'i':
-			MSG(__VER__);
+			PMSG(__VER__);
 			break;
 		case 's':
-			ds=0;
-			MSG("New start point: ");
-			for(i=0;i<8;i++){
-				u8 t;
-				t=sci_receive_byte();
-				MSG("%c",t);
-				ds*=16;
-				if(t>='0'&&t<='9'){
-					ds+=t-'0';
-				}else if(t>='a'&&t<='f'){
-					ds+=t-'a'+10;
-				}else if(t>='A'&&t<='F'){
-					ds+=t-'A'+10;
-				}else{
-					MSG("\n\rMalformed command. Reset start point.\n\r");
-					ds=0x00420000;
-					break;
-				}
-			}
-			//TODO check ds%4
-			MSG("\n\rStart point = 0x%08x now.\n\r",ds);
+			set_start_point();
 			break;
 		case 'l':
-			dl=0;
-			MSG("Enter data length: ");
-			for(i=0;i<4;i++){
-				u8 t;
-				t=sci_receive_byte();
-				MSG("%c",t);
-				dl*=16;
-				if(t>='0'&&t<='9'){
-					dl+=t-'0';
-				}else if(t>='a'&&t<='f'){
-					dl+=t-'a'+10;
-				}else if(t>='A'&&t<='F'){
-					dl+=t-'A'+10;
-				}else{
-					MSG("\n\rMalformed command.\n\r");
-					dl=0;
-					break;
-				}
-			}
-			if(dl==0||dl%4)
-				MSG("\n\rSet length failed. length should be divisible by 4 and not 0 \n\r");
-			else
-				MSG("\n\rSet length ok. New value is 0x%08x.\n\r",dl);
+			set_data_length();
 			break;
 		case 'w':
-			direct_write(ds,dl);
+			direct_write();
 			break;
 		default:
 			MSG("Unrecognized command - %c, Use 'h' to get help.\n\r",ser);
 		}
-		MSG("\n\rbootloader> ");
+		if(!is_programmer)MSG("\n\rbootloader> ");
 		//will block
 		ser=sci_receive_byte();
-		MSG("%c\n\r",ser);
+		if(!is_programmer)MSG("%c\n\r",ser);
 
 	}
 	return 0;
